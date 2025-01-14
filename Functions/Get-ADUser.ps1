bf7c5e8b5e64235de8eec8d4ef483e948043414b
ma1c0ntent <144507571+ma1c0ntent@users.noreply.github.com>
2025-01-14 07:54:24 -0600

    Update Get-ADUser.ps1

--- Functions/Get-ADUser.ps1
+++ Functions/Get-ADUser.ps1
@@ -1,121 +1,73 @@
-Function New-ADUser {
+Function Get-ADUser {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        [ValidateScript(
-            {
-                If ($_ -match "[a-zA-Z]") {
-                    return $true
-                }
-                Else {
-                    throw "First name must only contain alphabetic characters"
-                }
-            }
+        [Parameter(Mandatory = $true,
+        ParameterSetName = "Identity",
+        ValueFromPipeline = $true,
+        Position = 0,
+        HelpMessage = "Enter the SamAccountName of the user"
         )]
-        [string]$FirstName,
+        [string[]]$Identity,
 
-        [Parameter(Mandatory = $true)]
-        [ValidateScript(
-            {
-                If ($_ -match "[a-zA-Z]") {
-                    return $true
-                }
-                Else {
-                    throw "Last name must only contain alphabetic characters"
-                }
-            }
-        )]
-        [string]$LastName,
-
-        [Parameter(Mandatory = $false)]
-        [string]$DisplayName = "$FirstName $LastName",
-
-        [Parameter(Mandatory = $false)]
-        [string]$SamAccountName = $FirstName.Substring(0, 1) + $LastName,
-
+        [Parameter(Mandatory = $true,
+        ParameterSetName = "Filter")]
+        [string]$Filter,
         
-
-        [Parameter(Mandatory = $true)]
-        [ValidateScript(
-            {
-                If ($_ -match "^\w*\@\w*\.\w*$") {
-                    return $true
-                }
-                Else {
-                    throw "Email address is not valid"
-                    
-                }
-            }
-        )]
-        [string]$EmailAddress,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateScript(
-            {
-                If ($_ -match "[a-zA-Z0-9]") {
-                    return $true
-                }
-                Else {
-                    throw "Employee ID must only contain alphanumeric characters"
-                }
-            }
-        )]
-        [string]$EmpID,
-        
-        [Parameter(Mandatory = $true, HelpMessage = "Enter the department: HR, Security, Accounting, Marketing, Sales")]
-        [ValidateSet("HR", "Security", "Accounting", "Marketing", "Sales")]
-        [string]$Department,
-        
-        [Parameter(Mandatory = $true)]
-        [ValidateScript(
-            {
-                If ($_ -match '^"' -or $_ -match "^'") {
-                    throw "Job Title cannot contain quotes"
-                }
-                Else {
-                    return $true
-                }
-            }
-        )]
-        [string]$JobTitle,
-
         [Parameter(Mandatory = $false)]
-        [string]$Guid = [Guid]::NewGuid().ToString(),
-
-        [Parameter(Mandatory = $false)]
-        [string]$Enabled = "TRUE"
+        [string[]]$Properties
     )
-
     Begin {
         $database = Import-Csv -Path "$PSScriptRoot\..\Database\database.csv"
-        $datetime = Get-Date
-        $date = $datetime.ToShortDateString()
-        $time = $datetime.ToShortTimeString()
-        
-        $DC1 = $EmailAddress.Split("@")[1].Split(".")[0]
-        $DC2 = $EmailAddress.Split("@")[1].Split(".")[1]
-        
-        $DistinguishedName = "CN=$LastName,$FirstName,OU=Users,OU=$Department,DC=$DC1,DC=$DC2"
     }
     Process {
-        
-       $NewUser = [PSCustomObject] @{
-            'FirstName' = $FirstName
-            'LastName' = $LastName
-            'DisplayName' = $DisplayName
-            'SamAccountName' = $SamAccountName.ToLower()
-            'DistinguishedName' = $DistinguishedName
-            'EmailAddress' = $EmailAddress
-            'EmpID' = $EmpID
-            'JobTitle' = $JobTitle
-            'Guid' = $Guid
-            'Created' = "$date $time"
-            'Modified' = "$date $time"
-            'Enabled' = $Enabled
+        If ($PSCmdlet.ParameterSetName -eq "Filter") {
+
+            If ($Filter -eq "*") {
+                return $database | Select-Object FirstName, LastName, DisplayName, SamAccountName
+            }
+            Else {
+                $param1, $param2, $param3 = $filter.Split(' ')
+                $Expression = "`$database | Where-Object { `$_.$param1 $param2 $param3}"
+                return Invoke-Expression -Command $Expression
+                Break
+            }
         }
-        $Database += $NewUser
-    }
-    End {
-        $Database | Export-Csv -Path "$PSScriptRoot\..\Database\database.csv" -NoTypeInformation
+        Else {
+            If ($Identity -and (-not($Properties))) {
+                
+                If ($Identity -eq "*") {
+                    return $database | Select-Object FirstName, LastName, DisplayName, SamAccountName
+                }
+                
+                $FoundUser = $database | Where-Object { $_.SamAccountName -match $Identity }
+                If ($FoundUser) {
+                    $User = [PSCustomObject] @{
+                        'FirstName' = $FoundUser.'FirstName'
+                        'LastName'  = $FoundUser.'LastName'
+                        'DisplayName' = $FoundUser.'DisplayName'
+                        'SamAccountName' = $FoundUser.'SamAccountName'
+                    }
+                    return $User
+                }
+                Else {
+                    Write-Warning "User not found"
+                    Break
+                }
+            }
+            ElseIf (($Identity -and $Properties)) {
+                If ($Identity -eq "*") {
+                    return $database | Select-Object -Property $Properties
+                }
+                
+                $FoundUser = $database | Where-Object { $_.SamAccountName -in $Identity }
+                If ($FoundUser) {
+                    return $FoundUser | Select-Object -Property $Properties
+                }
+                Else {
+                    Write-Warning "User not found"
+                    Break
+                }
+            }
+        }
     }
 }
